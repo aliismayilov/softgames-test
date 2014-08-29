778c1bd50ad9c98506ff340d1a27a4c9cc93389b
Ali Ismayilov <ali@ismailov.info>
2014-08-28 21:23:42 -0300

    Task #1 about prime numbers completed

--- app/assets/javascripts/prime.js
+++ app/assets/javascripts/prime.js
@@ -1,11 +1,62 @@
 angular.module('primeApp', [])
   .controller('PrimeController', ['$scope', function($scope) {
+    var primeNumbers;
     var _initialize = function() {
       $scope.number = 13195;
-    }
+      $scope.result = [];
+      primeNumbers = [2, 3];
+    };
 
-    $scope.$watch('number', function(number) {
-      $scope.result = number + 1;
+    var isPrime = function(num) {
+      if (num <= 3) {
+        return true;
+      }
+      if ((num & 1) == 0) {
+        return false;
+      }
+      var sqrt = Math.sqrt(num);
+      for (var i = 3; i <= sqrt; i += 2) {
+        if (num % i == 0) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    var nextPrimeAfter = function(num) {
+      if (primeNumbers.indexOf(num) < primeNumbers.length - 1) {
+        return primeNumbers[primeNumbers.indexOf(num) + 1];
+      }
+      else {
+        num += 2;
+        while(!isPrime(num)) {
+          num += 2;
+        }
+        primeNumbers.push(num);
+        return num;
+      }
+    };
+
+    $scope.$watch('number', function(userNumber) {
+      if (Number.isInteger(userNumber)) {
+        var number = userNumber;
+        $scope.result = [];
+        for (var currentPrime = primeNumbers[0]; currentPrime <= number; currentPrime = nextPrimeAfter(currentPrime)) {
+          if (isPrime(number)) {
+            $scope.result.push(number);
+            break;
+          }
+          else {
+            if (number % currentPrime == 0) {
+              $scope.result.push(currentPrime);
+              number /= currentPrime;
+            }
+          }
+        }
+      }
+      else {
+        $scope.result = [];
+      }
     });
 
     _initialize();
