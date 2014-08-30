1afbdfc5007615b569be6dece65f502cfdfca4c5
Ali Ismayilov <ali@ismailov.info>
2014-08-30 18:22:11 -0300

    List albums from Picasa

--- app/assets/javascripts/picasaApp/js/controllers.js
+++ app/assets/javascripts/picasaApp/js/controllers.js
@@ -5,7 +5,13 @@
 angular.module('picasaApp.controllers', [])
   .controller('AlbumsCtrl',
   ['$scope', 'picasaFactory', function($scope, picasaFactory) {
-    console.log(picasaFactory.listAlbums());
+    var _initialize = function() {
+      picasaFactory.listAlbums().success(function (data) {
+        $scope.albums = data.feed.entry;
+      });
+    };
+
+    _initialize();
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
--- app/assets/javascripts/picasaApp/js/services.js
+++ app/assets/javascripts/picasaApp/js/services.js
@@ -5,15 +5,18 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('picasaApp.services', []).
-  factory('picasaFactory', ['$http', function($http){
+  factory('picasaFactory',
+  ['$http', '$rootScope', function($http, $rootScope){
     var factory = {};
 
     factory.listAlbums = function() {
-      $http.get('https://picasaweb.google.com/data/feed/api/user/default')
-      .success(function(data) {
-        return data;
+      return $http.get('https://picasaweb.google.com/data/feed/api/user/default', {
+        params: {
+          access_token: $rootScope.access_token,
+          alt: 'json'
+        }
       });
     };
 
     return factory;
-  }])
+  }]);
