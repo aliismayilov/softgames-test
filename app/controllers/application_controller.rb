b249b0ed75262fa7cf882a5377a7832919e48621
Ali Ismayilov <ali@ismailov.info>
2014-08-30 03:59:16 -0300

    Add ApplicationController helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,32 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :save_return_path
+  helper_method :current_user, :user_signed_in?
+
+  protected
+    def save_return_path
+      session[:return_path] = request.path
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      session.delete(:user_id)
+      nil
+    end
+
+    def user_signed_in?
+      current_user.present?
+    end
+
+    def authenticate!
+      return if user_signed_in?
+
+      origin = request.post? ? root_url : request.fullpath
+
+      redirect_to(login_url(provider: 'google_oauth2', origin: origin),
+        notice: 'You need to sign in...')
+    end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+describe ApplicationController do
+  let(:user) { create(:user) }
+
+  describe '#current_user' do
+    context 'user signed in' do
+      before do
+        session[:user_id] = user.id
+      end
+
+      it 'returns the user' do
+        expect(controller.send(:current_user)).to eql user
+      end
+    end
+
+    context 'user signed in with invalid id' do
+      before do
+        session[:user_id] = 666
+      end
+
+      it 'deletes user_id from session' do
+        expect {
+          controller.send(:current_user)
+        }.to change { session[:user_id] }.from(666).to(nil)
+      end
+
+      it 'returns nil' do
+        expect(controller.send(:current_user)).to be_nil
+      end
+    end
+
+    context 'user is not signed in' do
+      it 'returns nil' do
+        expect(controller.send(:current_user)).to be_nil
+      end
+    end
+  end
+
+  describe '#authenticate!' do
+    context 'user is signed in' do
+      before do
+        session[:user_id] = user.id
+      end
+
+      it 'ignores redirection and proceeds' do
+        expect(controller).to_not receive(:redirect_to)
+        controller.send :authenticate!
+      end
+    end
+
+    context 'user is not signed in' do
+      it 'redirects to login' do
+        redirect = double('redirect')
+        request.define_singleton_method :fullpath do
+          '/potato'
+        end
+
+        expect(controller)
+          .to receive(:redirect_to)
+          .with(login_url(provider: 'google_oauth2', origin: '/potato'),
+                notice: 'You need to sign in...')
+          .and_return(redirect)
+
+        expect(controller.send(:authenticate!)).to eql redirect
+      end
+    end
+  end
+end
